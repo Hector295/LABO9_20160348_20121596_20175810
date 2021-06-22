f98dd9c1d907a5f11c1d486f3a16de7c9ba43ce9
Fabrizio <a20160348@pucp.edu.pe>
2021-06-22 01:23:57 -0500

    Actividad: parte d y h

--- src/main/java/com/example/lab9/controller/ActividadWebService.java
+++ src/main/java/com/example/lab9/controller/ActividadWebService.java
@@ -1,15 +1,19 @@
 package com.example.lab9.controller;
 
 import com.example.lab9.entity.Actividades;
+import com.example.lab9.entity.Proyecto;
 import com.example.lab9.repository.ActividadRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
+import java.util.Optional;
 
 @RestController
 @CrossOrigin
@@ -23,7 +27,7 @@ public class ActividadWebService {
     }
 
     @PostMapping(value = "/actividad", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity guardarActividades(
+    public ResponseEntity guardarActividad(
             @RequestBody Actividades actividades,
             @RequestParam(value = "fetchId", required = false) boolean fetchId) {
 
@@ -38,5 +42,90 @@ public class ActividadWebService {
 
     }
 
+    @PutMapping(value = "/actividad", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity actualizarActividad(@RequestBody Actividades actividades) {
+
+        HashMap<String, Object> responseMap = new HashMap<>();
+
+        if (actividades.getIdactividad() > 0) {
+            Optional<Actividades> opt = actividadRepository.findById(actividades.getIdactividad());
+            if (opt.isPresent()) {
+                Actividades actOpt = opt.get();
+
+                if (actividades.getNombreactividad() != null &&
+                        !actOpt.getNombreactividad().equals(actividades.getNombreactividad())) {
+                    actOpt.setNombreactividad(actividades.getNombreactividad());
+                }
+                if (actividades.getUsuario_owner() != null &&
+                        actOpt.getUsuario_owner().equals(actividades.getUsuario_owner())) {
+                    actOpt.setUsuario_owner(actividades.getUsuario_owner());
+                }
+                if (actividades.getIdactividad() != 0 &&
+                        actOpt.getIdactividad() != actividades.getIdactividad()) {
+                    actOpt.setIdactividad(actividades.getIdactividad());
+                }
+                if (actividades.getIdproyecto() != 0 &&
+                        actOpt.getIdproyecto() != actividades.getIdproyecto()) {
+                    actOpt.setIdproyecto(actividades.getIdproyecto());
+                }
+                if (!actOpt.getDescripcion().equals(actividades.getDescripcion())) {
+                    actOpt.setDescripcion(actividades.getDescripcion());
+                }
+                if (actividades.getPeso() != null &&
+                        actOpt.getPeso().equals(actividades.getPeso())) {
+                    actOpt.setPeso(actividades.getPeso());
+                }
+                actividadRepository.save(actOpt);
+                responseMap.put("estado", "actualizado");
+                return new ResponseEntity(responseMap, HttpStatus.OK);
+            } else {
+                responseMap.put("estado", "error");
+                responseMap.put("msg", "El id de la actividad a actualizar no existe");
+                return new ResponseEntity(responseMap, HttpStatus.BAD_REQUEST);
+            }
+        } else {
+            responseMap.put("estado", "error");
+            responseMap.put("msg", "Debe enviar un ID");
+            return new ResponseEntity(responseMap, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @DeleteMapping(value = "/actividad/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity borrarProyecto(@PathVariable("id") String idrec) {
+
+        HashMap<String, Object> responseMap = new HashMap<>();
+
+        try {
+            int id = Integer.parseInt(idrec);
+            if (actividadRepository.existsById(id)) {
+                actividadRepository.deleteById(id);
+                responseMap.put("estado", "borrado exitoso");
+                return new ResponseEntity(responseMap, HttpStatus.OK);
+            } else {
+                responseMap.put("estado", "error");
+                responseMap.put("msg", "no se encontró la actividad con id: " + id);
+                return new ResponseEntity(responseMap, HttpStatus.BAD_REQUEST);
+            }
+        } catch (NumberFormatException ex) {
+            responseMap.put("estado", "error");
+            responseMap.put("msg", "El ID debe ser un número");
+            return new ResponseEntity(responseMap, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity gestionExcepcion(HttpServletRequest request) {
+
+        HashMap<String, Object> responseMap = new HashMap<>();
+        if (request.getMethod().equals("POST")) {
+            responseMap.put("estado", "error");
+            responseMap.put("msg", "Debe enviar una actividad");
+        }
+        if (request.getMethod().equals("PUT")) {
+            responseMap.put("estado", "error");
+            responseMap.put("msg", "Debe enviar una actividad");
+        }
+        return new ResponseEntity(responseMap, HttpStatus.BAD_REQUEST);
+    }
 
 }
